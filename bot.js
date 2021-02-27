1bff6d7d850db06ca0f74c5088c7516a94fc9852
Turtled <trtld2@gmail.com>
2021-02-27 02:52:16 -0800

    V1.0
    Basic bot that checks for a free account every 24hrs and reports results after saving user cookies.

---
+++ bot.js
@@ -0,0 +1,96 @@
+const puppeteer = require('puppeteer');
+const fs = require('fs')
+const readline = require("readline");
+const input = require('readline-sync');
+let doPrizeCheck = require("./prizeCheck");
+
+doSetup();
+
+function doSetup() {
+
+    if (!fs.existsSync("lastcheck.txt")) {
+        //first time running program, create file. set last checked to 24+ hours ago so we check now
+        fs.writeFile('./lastcheck.txt', Date.now() - 86409119, error => {
+            if (error) {
+                console.error(error);
+                return;
+            }
+        })
+    }
+
+    //check if we've saved cookies yet, if we haven't open a browser and have user log in with discord
+    if (fs.existsSync("cookies.json")) {
+        startPrizeCheckLoop();
+    } else {
+        doUserAuth();
+    }
+
+}
+
+function doUserAuth() {
+
+    console.log("do user auth");
+
+    let login = async () => {
+        console.log("asynch");
+        const browser = await puppeteer.launch({ headless: false });
+        const page = await browser.newPage();
+
+        await page.goto('https://turbosmurfs.gg/free-lol-account');
+
+        async function readLine() {
+
+            const rl = readline.createInterface({
+                input: process.stdin,
+                output: process.stdout
+            });
+
+            return new Promise(resolve => {
+
+                rl.question('\n\nThis bot requires you to log in with Discord on turbosmurfs.gg so it can save the cookies and use them in the future. \n\nThis bot will not save your password or username, and saved cookies can be deleted at any time (delete cookies.json). \n\n login with Discord on turbosmurfs.gg in the newly opened chrome window, then press enter here when finished.\n\n', (input) => {
+                    rl.close();
+                    resolve(input)
+                });
+            })
+        }
+
+        await readLine();//wait for user to press enter in console before continuing
+
+        const cookiesObject = await page.cookies('https://turbosmurfs.gg/free-lol-account')
+
+        fs.writeFile("cookies.json", JSON.stringify(cookiesObject),
+            function (err) {
+                if (err) {
+                    console.log(`\n\nError saving session cookies.\n\n`);
+                }
+                console.log(`\n\nSuccessfully saved session cookies.\n\n`);
+
+                browser.close();
+
+                startPrizeCheckLoop();
+            })
+    }
+
+    login();
+}
+
+function startPrizeCheckLoop() {
+    fs.readFile('lastcheck.txt',
+        { encoding: 'utf8', flag: 'r' },
+        function (err, data) {
+            if (err)
+                console.log(err);
+            else {
+                if (Date.now() - data > 86408519) {
+                    console.log("\n\nDoing prize check now.\n\n")
+                    //it has been 24 hours since last check
+                    doPrizeCheck();
+                }
+                else {
+                    console.log("\n\nHas not been 24 hours since last check. Doing next prize check in " + (((86400000 - (Date.now() - data)) / 1000) / 60) / 60  + " hours.\n\n")
+                    //has not been 24 hours, wait remaining time then check
+                    setTimeout(doPrizeCheck, 86400000 - (Date.now() - data));
+                }
+            }
+        });
+}
---
+++ prizeCheck.js
@@ -0,0 +1,67 @@
+const fs = require('fs')
+const puppeteer = require('puppeteer');
+const player = require('node-wav-player');
+
+let doPrizeCheck = async () => {
+
+    console.log("\n\nChecking prize...\n\n")
+
+    const browser = await puppeteer.launch({ headless: true });
+    const page = await browser.newPage();
+
+    const cookiesString = fs.readFileSync("cookies.json");
+    const parsedCookies = JSON.parse(cookiesString);
+
+    if (parsedCookies.length !== 0) {
+        for (let cookie of parsedCookies) {
+            await page.setCookie(cookie);
+        }
+    }
+    await page.goto('https://turbosmurfs.gg/free-lol-account');
+    await page.waitFor(5000);
+
+    await page.click('#open_lootbox');
+
+    await page.waitFor(15000);
+
+    const result = await page.evaluate(() => {
+        let resultElement = document.querySelector('#case-reward span')
+
+        let resultText = resultElement ? resultElement.innerText : null;
+        return resultText;
+    });
+
+    if (!result || result.indexOf("%") === -1) {
+        //we got a free acc
+        console.log("\n\nFree account! A screen shot of the prize result has been saved to file.\n\n")
+        await page.screenshot({ path: 'freeacc.png', fullPage: true });
+
+        player.play({
+            path: 'freeaccalarm.wav',
+        }).then(() => {
+        }).catch((error) => {
+            console.error(error);
+        });
+
+    }
+    else {
+        console.log("\n\nNot a free account, but you got: " + result + "\n\n")
+    }
+
+    browser.close();
+
+    //record last time we checked prize
+    fs.writeFile('lastcheck.txt', Date.now(), error => {
+        if (error) {
+            console.error(error);
+            return;
+        }
+    })
+
+    //reschedule this same function, in 24 hours
+    console.log("\n\nSuccessfully checked prize, new prize check scheduled to run in 24 hours.\n\n")
+    setTimeout(doPrizeCheck, 86408000);
+
+};
+
+module.exports = doPrizeCheck;
